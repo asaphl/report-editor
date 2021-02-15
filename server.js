d20efd3c733ec4c78bd902c5fecb50de3ab38437
asaphl <asaphlevy86@gmail.com>
2021-02-15 10:49:23 +0200

    Setup public directory with app images

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
+app.use(express.static('public'));
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));

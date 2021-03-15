a4ad87435747ee054e5b7b63b52e633df52ea7d5
asaphl <asaphlevy86@gmail.com>
2021-03-15 18:37:04 +0200

    retired sqlite db and settings

--- db/indexSqlite.js
+++
@@ -1,8 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-
-const db = new sqlite3.Database('./db/db.sqlite', err => {
-  if (err) console.log(err);
-  console.log('connected to db');
-});
-
-module.exports = db;

57fc8767af88bf9f3714ca09abf10de0f7b240fb
asaphl <asaphlevy86@gmail.com>
2021-03-15 10:29:53 +0200

    Updated empty document style

--- client/src/components/Report.jsx
+++ client/src/components/Report.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Block from "./Block";
 import { Droppable } from "react-beautiful-dnd";
+import { Box, makeStyles, Paper } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  emptyDocument: {
+    padding: '50px'
+  }
+});
 
 function Report(props) {
+  const classes = useStyles();
   const blocks = useSelector((state) => state.blocks);
   const renderBlocks = () => {
     if (!blocks.length)
       return (
-        <div>Open a report, or start dragging blocks to start editing</div>
+        <Box className={classes.emptyDocument}>Open a report, or start dragging blocks to start editing</Box>
       );
     return blocks.map((block, i) => (
       <Block {...block} index={i} key={block.id} />

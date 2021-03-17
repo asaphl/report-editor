03fe6d514f24e7276dcacf54523059e3abf3dd01
asaphl <asaphlevy86@gmail.com>
2021-03-17 11:30:50 +0200

    added release client

--- db/queryDB.js
+++ db/queryDB.js
@@ -1,13 +1,10 @@
 const db = require("../db");
 
 async function queryDB(sql) {
-  db.connect();
-  return new Promise(resolve => (
-    db
-    .query(sql)
-    .then(data => resolve(data.rows))
-    .catch(err => console.log(err))
-  ));
+  const client = await db.connect();
+  const { rows } = await client.query(sql);
+  client.release(true);
+  return rows;
 }
 
 module.exports = queryDB;
